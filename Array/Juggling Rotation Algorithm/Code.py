3402f869c32aaeb095ecb68d0511f03968ee705e
ConanKapoor <kapoor.shivam88@gmail.com>
2017-08-16 01:57:44 +0530

    Array: Juggling Algorithm #1

---
+++ Array/Juggling Rotation Algorithm/Code.py
@@ -0,0 +1,12 @@
+# Author - Shivam Kapoor
+# This code is written in accordance of algorithm given in geeksforgeeks.
+# Link - http://www.geeksforgeeks.org/array-rotation/
+
+from fractions import gcd
+
+# USELESS AS FUCK ALGORITHM OMFG
+
+#Inputing Data
+arr = input("Please input the array elements with spaces in between(ex - 1 2 3 ...): ").split()
+elements = int(input("Please input the number of elements to be moved at a time: "))
+gcd(len(arr),elements)
