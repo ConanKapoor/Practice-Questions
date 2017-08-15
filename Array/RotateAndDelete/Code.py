2eaff7fd0dc11f32f6350e9abb79b6d2aafd6a1d
ConanKapoor <kapoor.shivam88@gmail.com>
2017-08-15 22:12:25 +0530

    Array : RotationAndDeletion #3

--- Array/RotateAndDelete/Code.py
+++ Array/RotateAndDelete/Code.py
@@ -1,17 +1,20 @@
 #Author - Shivam Kapoor
 
+#Rotating array clockwise and performing deletion.
+def rotate(arr):
+    count = 1
+    while (len(arr) > 1):
+        arr = [arr[(i+1) % len(arr)] for i in range(0, len(arr))]
+        arr.pop(len(arr)-count) if len(arr)>count else arr.pop()
+        count+=1
+    arr1.append(arr[0])
+
 #Taking input
+arr1 = []
 Test_Cases = int(input())
 for test in range(0,Test_Cases):
     Terms = int(input())
     arr = input().split()
+    rotate(arr)
 
-#Rotating array clockwise and performing deletion.
-count = 1
-while (len(arr) > 1):
-    arr = [arr[(i+1) % len(arr)] for i in range(0, len(arr))]
-    arr.pop(count-1) if len(arr)>count else arr.pop(len(arr)-1)
-    count+=1
-
-#Priting result
-print(arr)
+print ("\n".join(arr1))
